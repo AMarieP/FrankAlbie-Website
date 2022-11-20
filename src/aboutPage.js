a852e186e2cf3499e635662b151392b05d0661fa
Alyssa <alyssa@kvasl.com>
2022-11-20 15:22:46 +1300

    About page can now grab the content from sanity

--- src/aboutPage.js
+++ src/aboutPage.js
@@ -1,33 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import sanityClient from "./client";
+import { PortableText } from '@portabletext/react'
+
 
 function AboutPage(){
 
     const[aboutContent, setAboutContent] = useState(null);
 
-
-
     useEffect(()=>{
         sanityClient
         .fetch(
-            `*[_type == 'content' && title == "About Me Page"]{
+            `*[_type == 'content' && title == "aboutme"]{
                 title, paragraph
             }`
         )
-        .then((data) => setAboutContent(data))
+        .then((data) => setAboutContent(data[0]))
         .catch(console.error);
     },);
+
+    if (!aboutContent) return <div>Loading...</div>; //if data is not loaded return a loading screen
 
     return(
         <>
         <h1>{aboutContent.title}</h1>
         <p>Text for about page</p>
         <img src="#"/>
-        <p>wanna c more</p>
+        <p>
+            <PortableText 
+            value={aboutContent.paragraph}
+            />
+        </p>
         </>
 
-    )
+    );
 }
 
 export default AboutPage;
