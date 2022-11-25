5445b57a0432365271dd77fe7cf812bf62d8051a
Alyssa <alyssa@kvasl.com>
2022-11-25 21:02:07 +1300

    p

--- src/components/addToCartButton.js
+++ src/components/addToCartButton.js
@@ -2,12 +2,15 @@ import { CartContext } from "../context/cart/cartContext";
 import { ActiveProductContext } from "../context/activeProduct/activeProductContext";
 import React, { useContext, useState } from "react";
 
+//Check if in arr already, if in arr increment quantity instead of products
+
 function AddToCartButton(){
     const {cartContents, setCart} = useContext(CartContext);
     const {currentProduct} = useContext(ActiveProductContext);
+
     function addToCart(product){
-        let b = cartContents.push(product);
-        console.log(cartContents);
+        let b = cartContents.push(product)
+        console.log(cartContents)
 
     };
 
--- src/context/cart/cart.js
+++ src/context/cart/cart.js
@@ -1,51 +1,72 @@
 import { CartContext } from "./cartContext";
 import { ActiveProductContext } from "../activeProduct/activeProductContext";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { Link } from 'react-router-dom'
+
 
 
 
 export default function Cart(){
-    const {cartContents, setCart} = useState(CartContext[0]);
+    const {cartContents, setCart} = useContext(CartContext);
+    //gets rid of first value defualts in context
+    const firstVal = cartContents.shift();
 
-    
-    // //adds a product to cart
-    // function addToCart(product){
-    //     setCart([
-    //         cartContents,
-    //         {
-    //             id: product._id,
-    //             prodName: product.title,
-    //             prodQuant: product.quantitiy,
-    //             cartQuant: 1,
-    //             prodPrice: product.price,
-    //         }
-    //     ])
-    // };
 
-    // //filters all contents except for the product id that is passed to delete a product from the cart arr.
-    // function removeFromCart(productId){
-    //     setCart(
-    //         cartContents.filter((t) => t.id ==! productId)
-    //         )
-    // };
 
-    // function incrementProduct(productId, cartQuant, prodQuant){
-    //     if(prodQuant > cartQuant){
-    //         cartQuant = cartQuant + 1;
-    //     }else if(prodQuant <= cartQuant){
-    //         cartQuant = cartQuant
-    //     }
-    // };
+    //Total of cart
+    let total = cartCostTotal();
+    function cartCostTotal(){
+        let t = 0;
+        cartContents.map((product) =>{
+            t += product.price
+            console.log(total)
+            return t
+        })
+        return t
+    }
 
-    // function cartCostTotal(){
-    //     let total = 0;
-    //     let prices = cartContents.map(cartContents.prodPrice * cartContents.quantitiy);
-    //     prices.forEach(() => total += prices[0])
 
-    // }
+    //filters all contents except for the product id that is passed to delete a product from the cart arr.
+    function removeFromCart(productId){
+        setCart(
+            cartContents.filter((t) => t.id ==! productId)
+            )
+    };
+
+    function incrementProduct(productId, cartQuant, prodQuant){
+        if(prodQuant > cartQuant){
+            cartQuant = cartQuant + 1;
+        }else if(prodQuant <= cartQuant){
+            cartQuant = cartQuant
+        }
+    };
+
+
+    let cartListings = "";
+    if(cartContents.length == 0){
+        cartListings = "There are no items in your cart!"
+    } else{
+        for(let i = 1; i < cartContents.length; i++){
+            cartListings += "sjhffhsdkjfh"
+        }
+    }
+
     
     return(
         <>
+        {cartContents &&
+            cartContents.map((product, index) => (<>
+            <Link to={'/' + product.slug.current} key={index}>
+                <span key={index}>
+                    <h2>{product.title}</h2>
+                    <p>{product.price}</p>
+                    <img src={product.imageUrl} />
+                </span>
+            </Link> 
+            <button onClick={() => removeFromCart(product.id)}>Remove</button>
+</>
+            ))}
+            <p>Total: {total}</p>
         </>
     )
 }
